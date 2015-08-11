2b7fdd26ecc8d106662ec1e1d1d6b19a83ae89d9
Travis Crampton & Tim Kellogg <student@epicodus.com>
2015-08-11 10:48:48 -0700

    add name and length_of_time

--- lib/place.rb
+++ lib/place.rb
@@ -1,9 +1,27 @@
 class Place
-  define_method(:initialize) do |description|
-    @description = description
+
+  @@all_places = []
+
+  define_method(:initialize) do |name, description, length_of_time|
+    @name           = name
+    @description    = description
+    @length_of_time = length_of_time
   end
 
   define_method(:description) do
     @description
   end
+
+  define_method(:name) do
+    @name
+  end
+
+  define_method(:length_of_time) do
+    @length_of_time
+  end
+
+  define_method(:save) do
+    @@all_places.push(self)
+  end
+  # Place.new(name, description, length_of_time)
 end
--- spec/place_spec.rb
+++ spec/place_spec.rb
@@ -2,10 +2,31 @@ require('rspec')
 require('place')
 
 describe(Place) do
+
+  before() do
+    @test_place = Place.new("Disneyland", "happiest place on earth", 4)
+    @test_place.save()
+  end
+
   describe('#description') do
     it("Lets you add a description") do
-      test_place = Place.new("scrub the rhino")
-      expect(test_place.description()).to(eq("scrub the rhino"))
+      # test_place = Place.new("scrub the rhino")
+      expect(@test_place.description()).to(eq("happiest place on earth"))
     end
   end
+
+  describe('#name') do
+    it('returns the name of the place') do
+      expect(@test_place.name()).to(eq("Disneyland"))
+    end
+  end
+
+  describe('#length_of_time') do
+    it('returns the length of time at the place') do
+      expect(@test_place.length_of_time()).to(eq(4))
+    end
+  end
+
+
+
 end
